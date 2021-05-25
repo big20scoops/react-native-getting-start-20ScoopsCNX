6cc27f6b7f31ec67085aefa870f9bc4b528f69d5
Big@20Scoops <visava.c@20scoops.net>
2021-05-25 19:58:15 +0700

    Add navigation

---
+++ src/navigation/MyStack.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MyStack;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+const HomeScreen = () => {
+  return (
+    <Text>Hello World!</Text>
+  );
+};
+
+export default HomeScreen;
