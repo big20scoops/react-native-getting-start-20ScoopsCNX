452342176015b5d883b7b45733b5397a6e7513e7
Big@20Scoops <visava.c@20scoops.net>
2021-05-31 20:23:01 +0700

    Setting up Apollo

--- App.tsx
+++ App.tsx
@@ -2,17 +2,24 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { ThemeProvider } from 'styled-components'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import MyStack from './src/navigation/MyStack';
 import theme from './src/theme'
 
+const client = new ApolloClient({
+  uri: 'https://hero-graphql.herokuapp.com/graphql',
+  cache: new InMemoryCache()
+});
 
 const App = () => {
 
   return (
-    <ThemeProvider theme={theme}>
-      <MyStack />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <MyStack />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 };
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components/native';
+import { useQuery, gql } from '@apollo/client';
 
 const Container = styled.View`
   flex: 1;
@@ -7,9 +8,22 @@ const Container = styled.View`
 `;
 
 const HomeScreen = () => {
+
+  const { loading, error, data } = useQuery(HERO_LIST);
+
+  console.log('heroes ==>', data)
+
   return (
     <Container />
   );
 };
+
+const HERO_LIST = gql`
+  query {
+    heros {
+      name
+    }
+  }
+`;
 
 export default HomeScreen;
