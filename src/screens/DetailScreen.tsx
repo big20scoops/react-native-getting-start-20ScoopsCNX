81f6e9776790f382092612e514971e803a4d8ede
Big@20Scoops <visava.c@20scoops.net>
2021-06-10 14:12:01 +0700

    added logic to update title name

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,11 +1,18 @@
 import {useQuery} from '@apollo/client';
+import {StackNavigationProp} from '@react-navigation/stack';
 import gql from 'graphql-tag';
-import React, {useCallback} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {Text} from 'react-native-svg';
 import styled from 'styled-components/native';
 import Spinner from '../components/Spinner';
 import {useAppSelector} from '../redux/hooks';
 import heroSelector from '../redux/selectors/heroSelector';
+import {RootStackParamList} from './rootStackPrams';
+
+interface DetailScreenProps {
+  titleName: string;
+  navigation: StackNavigationProp<RootStackParamList, 'HEROES'>;
+}
 
 const Container = styled.View`
   flex: 1;
@@ -16,11 +23,15 @@ const Name = styled.Text`
   color: ${({theme}) => theme.colors.black};
 `;
 
-const DetailScreen = () => {
+const DetailScreen = ({navigation}: DetailScreenProps) => {
   const currentHero = useAppSelector(heroSelector.currentHero);
   const {loading, error, data} = useQuery(HERO_DETAIL, {
     variables: {currentHero},
   });
+
+  useEffect(() => {
+    navigation.setOptions({title: data?.hero?.name});
+  }, [data, navigation]);
 
   const renderDetail = useCallback(() => {
     const {name} = data.hero;
